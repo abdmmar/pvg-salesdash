73d38c40b25dc7fc899ec14eec1e40d34768fcaa
abdmmar <opowae909@gmail.com>
2023-01-03 17:52:58 +0700

    chore:
    - add absolute import
    - add import svg as component
    - add colors
    - add font
    - add apexchart

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "klikoo-secondary": {
+          20: " #FCFCFF",
+          80: "#F3F4FF",
+        },
+        "klikoo-primary": {
+          60: "#989EFF",
+          100: "#545DFF"
+        }
+      }
+    },
   },
   plugins: [],
 }
